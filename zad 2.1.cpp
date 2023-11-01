523cd1eafc2270032d3881c62e0be077c90d0954
s20053-pj-marcin-mitros <74461758+s20053-pj-marcin-mitros@users.noreply.github.com>
2023-11-01 20:04:00 +0100

    Add files via upload

---
+++ zad 2.1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Podaj wartosc: ";
+    cin >> n;
+
+    int a = 0; 
+
+    for (int i = 1; i <= n; i++)
+    {
+        int b = 0;
+
+        for (int j = 1; j <= i; j++)
+        {
+            b += j;
+        }
+
+        cout << "1";
+        for (int k = 2; k <= i; k++)
+        {
+            cout << " + " << k;
+        }
+
+        cout << " = " << b << endl;
+
+        a += b;
+        if (i < n)
+        {
+            cout << "";
+        }
+    }
+
+    cout << "Suma szeregu: 1";
+    int c = 1;
+    for (int i = 2; i <= n; i++) 
+    {
+        c += i;
+        cout << " + " << c ;
+    }
+
+    cout << " = " << a << endl;
+
+    return 0;
+}
