246c9032ffa233824223ed00ef320cfa3ba5a19c
s20053-pj-marcin-mitros <74461758+s20053-pj-marcin-mitros@users.noreply.github.com>
2023-11-01 20:05:14 +0100

    Add files via upload

---
+++ zad 2.2.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cout << "Podaj liczbe a: ";
+    cin >> a;
+    cout << "Podaj liczbe b: ";
+    cin >> b;
+
+    cout << "Pobrano liczby a == " << a << " oraz b == " << b << endl << endl;
+
+    cout << "Wiersz o dlugosci a: \n";
+    for (int i = 0; i < a; i++) 
+    {
+        cout << "*";
+    }
+    cout << endl;
+
+    cout << "Wiersz o dlugosci b:\n";
+    for (int i = 0; i < b; i++)
+    {
+        cout << "*\n";
+    }
+
+    cout << "Prostokat gwiazdek o wymiarach a na b\n";
+    for (int i = 0; i < a; i++) 
+    {
+        for (int j = 0; j < b; j++) 
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << "Obwod prostokata o wymiarach a na b\n";
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++) 
+        {
+            if (i == 0 || i == a - 1 || j == 0 || j == b - 1) 
+            {
+                cout << "*";
+            }
+            else 
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    cout << "Trojkat prostokatny rownoramienny z kontem prostym w lewym dolnym rogu\n";
+    for (int i = 1; i <= a; i++) 
+    {
+        for (int j = 1; j <= i; j++) 
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ zad 2.3.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int month;
+
+    cout << "Prosze podac liczbe w zakresie 1-12\n";
+
+    cin >> month;
+
+    switch (month)
+    {
+    case 1:
+        cout << "Styczen\n" << "W roku nieprzestepnym ma 31 dni\n";
+        break;
+    case 2:
+        cout << "Luty\n" << "W roku nieprzestepnym ma 28 dni\n";
+        break;
+    case 3:
+        cout << "Marzec\n" << "W roku nieprzestepnym ma 31 dni\n";
+        break;
+    case 4:
+        cout << "Kwiecien\n" << "W roku nieprzestepnym ma 30 dni\n";
+        break;
+    case 5:
+        cout << "Maj\n" << "W roku nieprzestepnym ma 31 dni\n";
+        break;
+    case 6:
+        cout << "Czerwiec\n" << "W roku nieprzestepnym ma 30 dni\n";
+        break;
+    case 7:
+        cout << "Lipiec\n" << "W roku nieprzestepnym ma 31 dni\n";
+        break;
+    case 8:
+        cout << "Sierpien\n" << "W roku nieprzestepnym ma 31 dni\n";
+        break;
+    case 9:
+        cout << "Wrzesien\n" << "W roku nieprzestepnym ma 30 dni\n";
+        break;
+    case 10:
+        cout << "Pazdziernik\n" << "W roku nieprzestepnym ma 31 dni\n";
+        break;
+    case 11:
+        cout << "Listopad\n" << "W roku nieprzestepnym ma 30 dni\n";
+        break;
+    case 12:
+        cout << "Grudzien\n" << "W roku nieprzestepnym ma 31 dni\n";
+        break;
+    default:
+        cout << "Zostala wybrana liczba spoza skali\n";
+        return 0;
+    }
+    if (month >= 4 && month <= 9)
+    {
+        cout << "Jest to miesiac sloneczny\n";
+
+    }
+    else
+    {
+        cout << "Jest to miesiac pochmurny\n";
+    }
+    return 0;
+
+}
